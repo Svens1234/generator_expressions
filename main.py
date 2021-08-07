251f3ffe5a6cc16b33ad4be7af8370099ea5d488
Svenden123 <buhrus1@yandex.ru>
2021-08-07 19:10:22 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+b = (i**2 for i in range(1,6))
+print(sum(b))
+#print(b)
+#print(next(b))
+#print(next(b))
+#print(next(b))
+#print(next(b))
+#print(next(b))
+#print(next(b))
+
+#for i in b:
+    #print(i)
+
+
+
+
+
+
+
